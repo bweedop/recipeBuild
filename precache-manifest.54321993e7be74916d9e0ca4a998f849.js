e4cf524ee7fe9696b5035d15e73a941d063235f1
bweedop <kbweedop@gmail.com>
2020-11-24 15:11:48 -0700

    initial commit

---
+++ precache-manifest.54321993e7be74916d9e0ca4a998f849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9b9626cb5bd848c62b894fb0c5919b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be25872d1cd90f43aa6",
+    "url": "/static/css/main.96bb7410.chunk.css"
+  },
+  {
+    "revision": "014227d1e43e759b4368",
+    "url": "/static/js/2.342e5f63.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.342e5f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be25872d1cd90f43aa6",
+    "url": "/static/js/main.03d894a0.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
